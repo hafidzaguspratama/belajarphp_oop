e43c6d20d6baafe5a82a8b45b5a09ee0bf5aa649
hafidzaguspratama <59552750+hafidzaguspratama@users.noreply.github.com>
2020-06-18 11:15:04 +0700

    Add files via upload

---
+++ oop3.php
@@ -0,0 +1,24 @@
+ <?php
+  /**
+  * Program memanfaatkan Program 10.2 untuk membuat form inputan
+sederhana.
+ **/
+ include "oop2.php";
+ echo "<html>
+
+ 	<head>
+ 		<title>Mahasiswa</title>
+ 	</head>
+
+ <body>";
+		 $form = new Form("","Input Form");
+		 $form->addField("txtnim", "Nim");
+		 $form->addField("txtnama", "Nama");
+		 $form->addField("txtalamat", "Alamat");
+		 echo "<h3>Silahkan isi form berikut ini :</h3>";
+		 $form->displayForm();
+
+ echo "</body>
+	</html>";
+
+ ?>
